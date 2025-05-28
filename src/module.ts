80c1a42f1ebde5112f52d165fa3d744743ac450d
Erica Phelps <erica@blackspork.com>
2025-05-28 00:05:36 -0500

    feat:Moved hook to hooks.ts

--- src/module.ts
+++ src/module.ts
@@ -1,28 +1,2 @@
-import { log, logError } from "./logging";
-import { MiniatureMapCanvasGroup } from './MiniatureMapCanvasGroup';
 import "./Settings";
-
-Hooks.once("canvasReady", () => {
-  try {
-    if (!canvas?.stage) return;
-    if (canvas.stage.getChildByName("MiniatureMapCanvasGroup")) return;
-
-    // Initialize Pixi DevTools if we are a debug build
-    if (__DEV__) {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-      (window as any).__PIXI_DEVTOOLS__ = {
-        stage: canvas.stage,
-        renderer: canvas?.app?.renderer
-      }
-      log("Pixi DevTools enabled.");
-    }
-
-    // TODO: Stage Manager integration
-    const canvasGroup = new MiniatureMapCanvasGroup();
-    canvas.stage.addChild(canvasGroup);
-
-    log("Canvas group initialized.");
-  } catch (err) {
-    logError(err as Error);
-  }
-})
+import "./hooks";
