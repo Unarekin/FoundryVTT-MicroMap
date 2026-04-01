303216450c48d8c5e1d420e39102d3bd7b9c4d0c
Erica Phelps <erica@blackspork.com>
2026-04-01 18:56:34 -0500

    fix:Fixes #21

--- src/hooks.ts
+++ src/hooks.ts
@@ -98,7 +98,6 @@ function updateMap(scene: Scene) {
   map.scene = settings.scene ?? "";
   map.shape = settings.shape;
   map.mask = settings.mask;
-  // TODO Probably the source for #21
   map.visible = settings.enable && !!(game.settings?.get(__MODULE_ID__, "show"));
   map.position = settings.position;
   map.showWeather = settings.showWeather;
